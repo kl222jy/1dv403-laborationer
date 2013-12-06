43ad09b6a735dfe99d86d5fbc41eb05cad591835
kl222jy <kl222jy@student.lnu.se>
2013-12-06 13:04:03 +0100

    synk för datorbyte
    elnätet är tydligen fortfarande instabilt..

--- 3-gameon/memory/js/memory.js
+++ 3-gameon/memory/js/memory.js
@@ -2,6 +2,25 @@
 /*global window, event, document, console */
 var KLOS = KLOS || {};
 
-KLOS.Memory = {
-
+KLOS.Memory = function () {
+    var boardSizeY, boardSizeX, cells, board, cell;
+    
+    boardSizeX = 4;
+    boardSizeY = 4;
+    
+    cells = KLOS.Memory.RandomGenerator.getPictureArray(boardSizeY, boardSizeX);
+    board = function () {
+    
+    };
+    cell = function (cellIndex) {
+        var aTag, imgTag;
+        
+        aTag = document.createElement("a");
+        aTag.setAttribute("href", "#");
+        
+        imgTag = document.createElement("img");
+        imgTag.setAttribute("href", cells[cellIndex] + ".png");
+        
+        aTag.appendChild(imgTag);
+    };
 };
